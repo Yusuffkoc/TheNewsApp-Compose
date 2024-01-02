6877e3bc8460a4e1913ee5a2f8fb6d8a75cc9f85
Yusuffkoc <48139038+Yusuffkoc@users.noreply.github.com>
2024-01-02 21:10:49 +0300

    Pager Page Editing

--- app/src/main/java/com/example/thenewsapp/MainActivity.kt
+++ app/src/main/java/com/example/thenewsapp/MainActivity.kt
@@ -13,12 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.example.thenewsapp.presentation.onboarding.OnBoardingScreen
 import com.example.thenewsapp.ui.theme.TheNewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window,false)
         installSplashScreen()
         setContent {
             TheNewsAppTheme {
